3c9fad8c1fb31bae9bea0f85290ed257e946ccb5
HAMADSAALEH <hamadsalehswabi@gmail.com>
2023-10-23 00:03:41 +0500

    retirement.c 3

--- retirement.c
+++ retirement.c
@@ -14,3 +14,7 @@ double calculate_monthly_interest(double balance, double monthly_interest_rate) 
 double calculate_new_balance(double balance, double monthly_contribution, double monthly_interest) {
   return balance + monthly_contribution + monthly_interest;
 }
+// Prints the amortization table for a 401(k) account
+void print_amortization_table(double initial_balance, double monthly_contribution, double annual_rate_of_return, double annual_rate_of_inflation, int number_of_years) {
+  double monthly_interest_rate = inflation_adjusted_rate_of_return(annual_rate_of_return, annual_rate_of_inflation) / 12;
+  double balance = initial_balance;
